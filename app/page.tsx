a902cb23e98ba72337764001f7dc5552da4bee29
sharshith241-svg <sharshith241@gmail.com>
2026-01-23 13:57:22 +0530

    sd

--- app/page.tsx
+++ app/page.tsx
@@ -271,7 +271,7 @@ export default function HomePage() {
       {/* Footer */}
       <footer className="bg-gray-900 text-gray-300 py-8 mt-16">
         <div className="max-w-7xl mx-auto px-4 text-center">
-          <p>&copy; 2024 Himalayan Runners. All rights reserved.</p>
+          <p>&copy; 2025 Himalayan Runners. All rights reserved.</p>
         </div>
       </footer>
     </div>
