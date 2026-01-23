3e7828540108ed88dc0b2838c7f6e73e24480711
sharshith241-svg <sharshith241@gmail.com>
2026-01-23 12:53:58 +0530

    did

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
-    <main style={{ padding: "2rem", background: "#f5f5f5" }}>
+    <main style={{ padding: "2rem", background: "#1d0505" }}>
       <h1>Admin Panel</h1>
       {children}
     </main>
