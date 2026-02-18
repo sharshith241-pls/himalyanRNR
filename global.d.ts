e0b358865133170b6003705e909c1f7cc24c204f
sharshith241-svg <sharshith241@gmail.com>
2026-02-18 12:04:59 +0530

    hshsh

---
+++ global.d.ts
@@ -0,0 +1,32 @@
+interface RazorpayOptions {
+  key_id: string | undefined;
+  order_id: string;
+  amount: number;
+  currency: string;
+  name: string;
+  description: string;
+  prefill: {
+    name: string;
+    email: string;
+  };
+  theme: {
+    color: string;
+  };
+  handler: (response: any) => void;
+  modal?: {
+    ondismiss?: () => void;
+  };
+}
+
+interface RazorpayInstance {
+  open(): void;
+  close(): void;
+}
+
+declare global {
+  interface Window {
+    Razorpay: new (options: RazorpayOptions) => RazorpayInstance;
+  }
+}
+
+export {};
